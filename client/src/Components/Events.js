49b88c2a8dcff643278d6d02645caa867328b30a
Greg Schuman <gcschuman@hotmail.com>
2020-11-07 13:04:50 -0800

    Added content to About.js and Events.js.

---
+++ client/src/Components/Events.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+
+function Events (){
+    return(
+        <div className="events-container">
+            <h1>Ucoming Matches and Events</h1>
+            <h2>Note: due to Covid-related restrictions, events are subject to change</h2>
+            <p></p>
+        <Footer />
+        </div>
+    )
+}
+export default Events
