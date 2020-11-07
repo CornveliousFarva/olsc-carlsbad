4b1bc2cfbbc8ecd1d1aa9a8643b6cf8688752717
Greg Schuman <gcschuman@hotmail.com>
2020-11-07 14:41:28 -0800

    Added react-mdl and added content to Home.js and added new page-main.js.

---
+++ client/src/Components/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Footer from './Footer'
+
+
+class Home extends Component {
+  render() {
+    return(
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+              alt="avatar"
+              className="avatar-img"
+              />
+
+            <div className="banner-text">
+              <h1>Official Liverpool FC Supporters Club - Carlsbad, California</h1>
+                <p>Club Motto Here</p>
+                <p></p>
+            <hr/>
+
+
+        <div className="social-links">
+
+        </div>
+            </div>
+            <Footer/>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Home;
---
+++ client/src/Components/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+import Events from './Events';
+import Pics from './Pics';
+
+
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/events" component={Events} />
+    <Route path="/pic" component={Pics} />
+  </Switch>
+)
+
+export default Main;
