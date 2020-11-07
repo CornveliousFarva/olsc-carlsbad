3c7b59a853214b8b04bf7a8e8c860bcdeafed195
Greg Schuman <gcschuman@hotmail.com>
2020-11-07 14:21:33 -0800

    Added routes to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,39 @@
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 import Footer from './Components/Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Carlsbad Kopites</Link>} scroll>
+            <Navigation>
+                <Link to="/resume">Events</Link>
+                <Link to="/about">About Us</Link>
+                <Link to="/pics">Pics</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Carlsbad Kopites</Link>}>
+            <Navigation>
+                <Link to="/resume">Events</Link>
+                <Link to="/about">About Us</Link>
+                <Link to="/pics">Pics</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+        <Footer/>
+    </Layout>
+</div>
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/About">About</Link>
-            </li>
-            <li>
-              <Link to="/Events">Events</Link>
-            </li>
-            <li>
-              <Link to="/Pics">Pics</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-      <Footer></Footer>
-    </Router>
-  );
+    );
+  }
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
+export default App;
