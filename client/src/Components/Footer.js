3cf1edfc92ca9429973254b7f361fc98c2051755
Greg Schuman <gcschuman@hotmail.com>
2020-11-07 16:02:35 -0800

    Added new links to the footer.

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -8,10 +8,15 @@ function Footer() {
                 <div className="row">
                     <h3>Social Media</h3>
                     <ui className="social-list">
-                        <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
+                        <li><i class="fa fa-instagram" aria-hidden="true">@socal_kopites</i></li>
+                        <li><i class="fa fa-twitter" aria-hidden="true">@SoCal_Kopites</i></li>
                         <li>Email <link></link></li>
                     </ui>
+                </div>
+                <div className="container">
+                    <h3>Sponsors</h3>
+                    {/* <p>Senor Grubby's <a href=""></a></p> */}
+                    <p></p>
                 </div>
             </div>
         </div>
