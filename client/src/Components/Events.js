c8ee5f6cf3e6f124a7a2e956e322c467fdb34b74
Greg Schuman <gcschuman@hotmail.com>
2020-11-13 20:08:53 -0800

    Added content to Events.js.

--- client/src/Components/Events.js
+++ client/src/Components/Events.js
@@ -5,8 +5,11 @@ function Events (){
     return(
         <div className="events-container">
             <h1>Ucoming Matches and Events</h1>
-            <h2>Note: due to Covid-related restrictions, events are subject to change</h2>
-            <p></p>
+            <h2>Note: due to Covid-related restrictions, game meetups are subject to change. Schedule is also subject to change due to league obligations</h2>
+            <p>Games</p>
+            <ul>
+                <l1></l1>
+            </ul>
         <Footer />
         </div>
     )
