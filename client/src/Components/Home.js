8901ff3d1e9de4bb5f42819cdcda4e09260d2fc0
Greg Schuman <gcschuman@hotmail.com>
2020-11-13 19:44:24 -0800

    Added navbar component and deleted unnecessary avatar from home.js.

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -9,22 +9,14 @@ class Home extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img
-              src=""
-              alt="avatar"
-              className="avatar-img"
-              />
-
+          
             <div className="banner-text">
+        
               <h1>Official Liverpool FC Supporters Club - Carlsbad, California</h1>
                 <p>Community | Unity | Passion</p>
                 <p>Representing supporters of Liverpool FC in Northern San Diego.</p>
             <hr/>
 
-
-        <div className="social-links">
-
-        </div>
             </div>
             <Footer/>
           </Cell>
