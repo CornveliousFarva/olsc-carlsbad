a90def87954296e694803d2827a94c91dbc11b28
Greg Schuman <gcschuman@hotmail.com>
2020-11-14 16:34:58 -0800

    Replaced react-mdl with reactstrap on aboutus.js and landingpage.js and added a new component: navbar.js.

--- client/src/Components/landingpage.js
+++ client/src/Components/landingpage.js
@@ -1,50 +1,29 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React from 'react';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
 
-
-class Landing extends Component {
-  render() {
-    return(
-      <div style={{width: '100%', margin: 'auto'}}>
-        <Grid className="landing-grid">
-          <Cell col={12}>
-            {/* Insert OLSC image here */}
-            {/* <img
-              src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
-              alt="avatar"
-              className="avatar-img"
-              /> */}
-
-
-            <div className="banner-text">
-              <h1>Official Liverpool FC Supporters Club- Carlsbad, California</h1>
-
-            <hr/>
-
-          <p>Community | Unity | Passion</p>
-          <p>Representing Liverpool FC fans in Northern San Diego</p>
-
-        <div className="social-links">
-
-          {/* Twitter */}
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-
-          {/* Instagram */}
-          <i class="fa fa-instagram" aria-hidden="true"></i>
-
-          {/* Facebook */}
-          <i class="fa fa-facebook" aria-hidden="true"></i>
-
-          {/* Senor Grubby's link */}
-
-          {/* McCann Studio link */}
-        </div>
-            </div>
-          </Cell>
-        </Grid>
-      </div>
-    )
-  }
-}
+const Landing = (props) => {
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">Official Liverpool FC Supporters Club- Carlsbad, California</h1>
+          <p className="lead"> Community | Unity | Passion</p>
+          <p>Representing Liverpool FC supporters in Northern San Diego</p>
+        </Container>
+      </Jumbotron>
+      <Container>
+      <Row>
+        <Col><i class="fa fa-instagram" aria-hidden="true"></i></Col>
+        <Col><i class="fa fa-facebook" aria-hidden="true"></i></Col>
+        <Col><i class="fa fa-twitter" aria-hidden="true"></i></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Landing;
+
