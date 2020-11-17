075a6fa3ddd04e90a62908d29a6e132625fcebf1
Greg Schuman <gcschuman@hotmail.com>
2020-11-16 21:36:41 -0800

    Changes the links to buttons. That seems to have helped with about.js and events.js, but not with the home page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import { DropdownToggle, DropdownMenu, } from 'reactstrap'
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Button,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
 import Main from './components/main';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Official Liverpool FC Supporters Club-Carlsbad, CA</Link>} scroll>
-            <Navigation>
-                <Link to="/about">About Us</Link>
-                <Link to="/events">Events</Link>
-                <Link to="/contact">Contact</Link>
-                <DropdownToggle>
-                  Pics
-                </DropdownToggle>
-                  <DropdownMenu>
-                    <Link to="/groupPics">Group Pics</Link>
-                    <Link to="/scarfPics">Scarf Pics</Link>
-                  </DropdownMenu>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Official Liverpool FC Supporters Club-Carlsbad, CA</Link>}>
-            <Navigation>
-                <Link to="/about">About Us</Link>
-                <Link to="/events">Events</Link>
-                <Link to="/contact">Contact</Link>
-                <DropdownToggle>
-                  Pics
-                </DropdownToggle>
-                  <DropdownMenu>
-                    <Link to="/groupPics">Group Pics</Link>
-                    <Link to="/scarfPics">Scarf Pics</Link>
-                  </DropdownMenu>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content" />
-            <Main/>
-        </Content>
-    </Layout>
-</div>
+const App = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    );
-  }
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/homepage">OLSC Carlsbad</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Button tag={Link} to="/about">About</Button>
+            </NavItem>
+            <NavItem>
+            <Button tag={Link} to="/events">Events</Button>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Pictures
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem href="/components/groupPics">
+                  Group Pics
+                </DropdownItem>
+                <DropdownItem href="/components/scarfPics">
+                  Scarf Pics
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <Main/>
+    </div>
+  );
 }
 
 export default App;
