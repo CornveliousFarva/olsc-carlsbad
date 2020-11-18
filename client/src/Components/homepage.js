3a09fdb63e856979bcbdec0e08375802c1ed2008
Greg Schuman <gcschuman@hotmail.com>
2020-11-17 18:30:58 -0800

    Trying some things in the CSS to try and keep the navbar at the top of the page when moving between pages.

--- client/src/Components/homepage.js
+++ client/src/Components/homepage.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
 
 const Landing = (props) => {
   return (
     <div class="homepage-div">
       <Jumbotron fluid>
         <Container fluid>
-          <h1 className="display-3">Official Liverpool FC Supporters Club- Carlsbad, California</h1>
-          <hr className="my-2" />
-          <h2> Community | Unity | Passion</h2>
-          <hr className="my-2" />
-          <h3>Representing Liverpool FC supporters in Northern San Diego</h3>
+          <Row>
+            <Col>
+              <h1 className="display-3">Official Liverpool FC Supporters Club- Carlsbad, California</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2> Community | Unity | Passion</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3>Representing Liverpool FC supporters in Northern San Diego</h3>
+            </Col>
+          </Row>
         </Container>
       </Jumbotron>
     </div>
@@ -18,4 +28,3 @@ const Landing = (props) => {
 };
 
 export default Landing;
-
