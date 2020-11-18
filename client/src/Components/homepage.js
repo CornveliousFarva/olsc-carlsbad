57a7172223999775c162787aa14c7edfe4aadc07
Greg Schuman <gcschuman@hotmail.com>
2020-11-17 17:56:05 -0800

    Made some changes in the homepage.

--- client/src/Components/homepage.js
+++ client/src/Components/homepage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, Container, Row, Col } from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 
 const Landing = (props) => {
   return (
@@ -9,14 +9,6 @@ const Landing = (props) => {
           <h1 className="display-3">Official Liverpool FC Supporters Club- Carlsbad, California</h1><br></br>
           <h2> Community | Unity | Passion</h2><br></br>
           <h3>Representing Liverpool FC supporters in Northern San Diego</h3>
-         <Row>
-        <Col><i class="fa fa-instagram" aria-hidden="true">Instagram:</i></Col>
-        <Col><i class="fa fa-facebook" aria-hidden="true">Facebok:</i></Col>
-        <Col><i class="fa fa-twitter" aria-hidden="true">Twitter:</i></Col>
-        <Col>Email:</Col>
-        <Col></Col>
-        <Col></Col>
-      </Row> 
         </Container>
       </Jumbotron>
     </div>
