be51c18330f0f0532ee49cae609a527cccafdcde
Greg Schuman <gcschuman@hotmail.com>
2020-11-18 20:34:59 -0800

    Restarted working on the structure of the footer and added some css.

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -6,24 +6,16 @@ import {
   faInstagram
 } from "@fortawesome/free-brands-svg-icons";
 
-function Footer() {
-  return (
-    <Container>
-      <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
-        <div class="container">
-          <Row>
-            <Col><FontAwesomeIcon icon={faTwitter} /></Col>
-            <Col><FontAwesomeIcon icon={faInstagram} /></Col>
-            <Col>.col</Col>
-            <Col>.col</Col>
-          </Row>
-        </div>
-      </footer>
-    </div>
-    </Container>
-    
-  );
+const Footer = (props) => {
+  <Container fluid>
+    <Row>
+        <Col><FontAwesomeIcon icon={faTwitter} size="2x"/></Col>
+        <Col><FontAwesomeIcon icon={faInstagram} size="2x"/>.col</Col>
+        <Col>.col</Col>
+        <Col>.col</Col>
+      </Row>
+  </Container>
 }
+
 
 export default Footer;
