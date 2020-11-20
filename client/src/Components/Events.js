1a62ac9860b492ce5e47db2e93d885915f42c485
Greg Schuman <gcschuman@hotmail.com>
2020-11-19 20:56:48 -0800

    Added table for events and am trying to figure out issues with the routing to the pictures pages.

--- client/src/Components/Events.js
+++ client/src/Components/Events.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Container } from 'reactstrap'
+import { Container,
+Table } from 'reactstrap'
 import Footer from './footer'
 
     function Events(){
@@ -7,9 +8,51 @@ import Footer from './footer'
             <Container fluid>
               <div className="events-container">
                 <h1>Upcoming Games and Events</h1>
-                <p>Note 1: Game times are subject to change.</p>
+                <p>Note 1: Game times are subject to change and are listed in Pacific Time.</p>
                 <p>Note 2: Due to Covid restrictions, event meetups are also subject to change based on local health guidelines</p>
-            </div>  
+            </div> 
+            <Table responsive>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Opponent</th>
+          <th>Match Time</th>
+          <th>Result</th>
+          <th>Notes</th>
+          <th>TV</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+          <td>Table cell</td>
+        </tr>
+      </tbody>
+    </Table>
             <Footer />
             </Container>
         )
