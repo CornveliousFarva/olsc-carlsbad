f88aea874863c5747757a7f898e1de3cac67b000
Greg Schuman <gcschuman@hotmail.com>
2020-11-23 21:23:20 -0800

    Made some minor grammatical revisions in about.js.

--- client/src/Components/about.js
+++ client/src/Components/about.js
@@ -10,7 +10,7 @@ const About = (props) => {
       <Row>
         <Col>
         <h1>About Us</h1>
-          <p>Established in 2016, the Official Liverpool FC Supporters Club-Carlsbad, California, serves the communities of northern San Diego County: Carlsbad, Oceanside, Vista, San Marcos, Escondido, Fallbrook, Encinitas, and Solana Beach. Our usual meeting place for match days-current restrictions pending is Senor Grubby's, located in the heart of downtown Carlsbad. While we represent northern San Diego, we are also glad to welcome and any all visiting Liverpool supporters from around the world to stop on by for a match if they're in the area.</p>
+          <p>Established in 2016, the Official Liverpool FC Supporters Club-Carlsbad, California, serves the communities of northern San Diego County: Carlsbad, Oceanside, Vista, San Marcos, Escondido, Fallbrook, Encinitas, and Solana Beach. Our usual meeting place for match days-current restrictions pending, is Senor Grubby's, located in the heart of downtown Carlsbad. While we represent northern San Diego, we are also glad to welcome and any all visiting Liverpool supporters from around the world to stop on by for a match if you're in the area.</p>
         </Col>
       </Row>
         <h2>Directions to Grubby's</h2>
@@ -26,13 +26,13 @@ const About = (props) => {
         </Col>
         <Col sm="4">
         <h3>From the East: Vista, San Marcos, and Escondido</h3>
-          <p>Due to congestion at the interchange between State Route 78 and I-5 South, thie is a more advisable route. Take State Route 78 West and get off on El Camino Real and make a left. Make a right onto Carlsbad Village Drive and follow it until you see the train tracks. Grubby's is just past the train tracks and is on the left hand side.</p>
+          <p>Take State Route 78 West and get off on El Camino Real and make a left. Make a right onto Carlsbad Village Drive and follow it until you see the train tracks. Grubby's is just past the train tracks and is on the left hand side. Due to frequent congestion at the interchange between State Route 78 and I-5 South, this is a more advisable route.</p>
         </Col>
       </Row>
       <Row>
         <Col>
         <h2>Parking</h2>
-          <p>There are several parking lots within walking distance, including one across the street. Parking at Grubby's is EXTREMELY limited.</p>
+          <p>There are several parking lots within walking distance, including one across the street. Parking at Grubby's itself is EXTREMELY limited.</p>
         </Col>
       </Row>
       <div className="footer-container">
