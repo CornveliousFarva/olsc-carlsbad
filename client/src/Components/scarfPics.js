c609395c0d0594d33eea5bdbfe51b66dbcfe0e11
Greg Schuman <gcschuman@hotmail.com>
2020-11-24 19:41:36 -0800

    Started rebuilding the group pics page.

--- client/src/Components/scarfPics.js
+++ client/src/Components/scarfPics.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React  from 'react';
 import {
   Container, Row, Col 
 } from 'reactstrap';
 import Footer from './footer'
-const image =  'client/src/images/scarf-design.PNG'
 
 
 const ScarfPics = (props) => {
@@ -12,13 +11,10 @@ const ScarfPics = (props) => {
       <Row>
         <Col><h1>Our Scarf</h1></Col>
       </Row>
-      <Row><img src={image} alt="scarf-design"></img>
+      <Row>
       </Row>
       <p>This is our scarf, respresenting the city of Carlsbad with the arches that feature the city's name, as well as Liverpool's 6 European Cups. From the beaches of California, to the cornfields of Iowa, and the Fields of Anfield Road, it has been many places.</p>
       <h2>More Pictures</h2>
-      <Row>
-        
-      </Row>
       <Footer />
     </Container>
   )
